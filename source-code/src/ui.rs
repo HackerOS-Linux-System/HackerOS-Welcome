eb785e5f00785288b89d530b2c4fe878c9a84c9b
michal92299 <chudybyczek2137@gmail.com>
2025-11-09 20:43:45 +0100

    Update ui.rs

--- source-code/src/ui.rs
+++ source-code/src/ui.rs
@@ -1,134 +1,145 @@
-// src/ui.rs
 use adw::prelude::*;
-use adw::{HeaderBar, ToolbarView};
-use gtk::prelude::*;
-use gtk::{Align, Box as GtkBox, Button, FlowBox, Image, Label, Orientation, PolicyType, ScrolledWindow, Separator};
-use glib::clone;
-use std::cell::RefCell;
-use std::path::Path;
+use adw::ApplicationWindow;
+use gtk::{Box as GtkBox, Button, Label, Orientation, ScrolledWindow, Separator};
 use std::rc::Rc;
-use gdk_pixbuf::Pixbuf;
+use std::cell::RefCell;
+
 use crate::actions::Actions;
 
-pub fn build_ui(window: &adw::ApplicationWindow, actions: Rc<RefCell<Actions>>) {
-    // HeaderBar
-    let header_bar = HeaderBar::builder()
-        .show_end_title_buttons(true)
-        .build();
+pub fn build_ui(window: &ApplicationWindow, actions_rc: Rc<RefCell<Actions>>) {
+    let main_box = GtkBox::builder()
+    .orientation(Orientation::Vertical)
+    .spacing(20)
+    .margin_start(20)
+    .margin_end(20)
+    .margin_top(20)
+    .margin_bottom(20)
+    .build();
 
-    // Title widget with logo and title
-    let title_box = GtkBox::builder()
-        .orientation(Orientation::Horizontal)
-        .spacing(10)
-        .build();
-
-    // Logo
-    let logo_path = "/usr/share/HackerOS/ICONS/Plymouth-Icons/watermark.png";
-    let logo_image = if Path::new(logo_path).exists() {
-        let pixbuf = Pixbuf::from_file_at_scale(logo_path, 32, 32, true).ok();
-        Image::from_pixbuf(pixbuf.as_ref())
-    } else {
-        Image::new()
-    };
-    title_box.append(&logo_image);
-
-    // Title label
+    // Title
     let title_label = Label::builder()
-        .label("Witaj w HackerOS!")
-        .use_markup(true)
-        .build();
-    title_label.set_markup("<span font='Arial bold 20'>Witaj w HackerOS!</span>");
-    title_box.append(&title_label);
+    .label("Witaj w HackerOS!")
+    .css_classes(vec!["title".to_string()])
+    .build();
+    main_box.append(&title_label);
 
-    header_bar.set_title_widget(Some(&title_box));
-
-    // Content box
-    let content_box = GtkBox::builder()
-        .orientation(Orientation::Vertical)
-        .spacing(15)
-        .margin_top(20)
-        .margin_start(20)
-        .margin_end(20)
-        .margin_bottom(20)
-        .build();
-
-    // Subtitle
+    // Subtitle (feedback label)
     let subtitle_label = Label::builder()
-        .label("Twój system do Gier i Etycznego Hakowania")
-        .use_markup(true)
-        .halign(Align::Center)
-        .build();
-    subtitle_label.set_markup("<span font='Arial 18'>Twój system do Gier i Etycznego Hakowania</span>");
-    subtitle_label.style_context().add_class("subtitle");
-    content_box.append(&subtitle_label);
+    .label("Wybierz akcję poniżej.")
+    .css_classes(vec!["subtitle".to_string()])
+    .build();
+    main_box.append(&subtitle_label);
 
-    actions.borrow_mut().subtitle_label = Some(subtitle_label.clone());
+    // Set subtitle_label in Actions
+    {
+        let mut actions = actions_rc.borrow_mut();
+        actions.subtitle_label = Some(subtitle_label);
+    }
 
     // Separator
-    let separator = Separator::builder()
-        .orientation(Orientation::Horizontal)
-        .margin_start(10)
-        .margin_end(10)
-        .build();
-    content_box.append(&separator);
+    let separator1 = Separator::new(Orientation::Horizontal);
+    main_box.append(&separator1);
 
-    // ScrolledWindow for buttons
+    // Scrolled content for buttons
     let scrolled_window = ScrolledWindow::builder()
-        .hscrollbar_policy(PolicyType::Never)
-        .vscrollbar_policy(PolicyType::Automatic)
-        .hexpand(true)
-        .vexpand(true)
-        .build();
-    content_box.append(&scrolled_window);
+    .vexpand(true)
+    .build();
 
-    // FlowBox for buttons
-    let flow_box = FlowBox::builder()
-        .column_spacing(15)
-        .row_spacing(15)
-        .margin_start(10)
-        .margin_end(10)
-        .margin_bottom(20)
-        .homogeneous(true)
-        .min_children_per_line(2)
-        .max_children_per_line(3)
-        .build();
-    scrolled_window.set_child(Some(&flow_box));
+    let buttons_box = GtkBox::builder()
+    .orientation(Orientation::Vertical)
+    .spacing(10)
+    .margin_start(10)
+    .margin_end(10)
+    .margin_top(10)
+    .margin_bottom(10)
+    .build();
 
     // Buttons
-    let buttons = vec![
-        ("Otwórz stronę HackerOS", clone!(@strong actions => move || actions.borrow().open_website())),
-        ("Otwórz X", clone!(@strong actions => move || actions.borrow().open_x())),
-        ("Otwórz sklep z aplikacjami", clone!(@strong actions => move || actions.borrow().open_software())),
-        ("Changelog", clone!(@strong actions => move || actions.borrow().open_changelog())),
-        ("Informacje o systemie", clone!(@strong actions => move || actions.borrow().open_system_info())),
-        ("Zgłoś błąd", clone!(@strong actions => move || actions.borrow().report_bug())),
-        ("Forum dyskusyjne", clone!(@strong actions => move || actions.borrow().open_forum())),
-        ("Zaktualizuj system", clone!(@strong actions => move || actions.borrow().update_system())),
-        ("Uruchom HackerOS Games", clone!(@strong actions => move || actions.borrow().run_command_with_feedback("/usr/share/HackerOS/Scripts/Bin/HackerOS-Games.sh"))),
-        ("Hacker Launcher", clone!(@strong actions => move || actions.borrow().run_command_with_feedback("/usr/share/HackerOS/Scripts/HackerOS-Apps/Hacker_Launcher"))),
-    ];
+    let website_button = Button::builder()
+    .label("Otwórz stronę HackerOS")
+    .build();
+    website_button.connect_clicked({
+        let actions_rc = actions_rc.clone();
+        move |_| actions_rc.borrow().open_website()
+    });
+    buttons_box.append(&website_button);
 
-    for (label, action) in buttons {
-        let button = Button::builder()
-            .label(label)
-            .build();
-        button.connect_clicked(move |_| action());
-        flow_box.append(&button);
-    }
+    let x_button = Button::builder()
+    .label("Otwórz X (Twitter)")
+    .build();
+    x_button.connect_clicked({
+        let actions_rc = actions_rc.clone();
+        move |_| actions_rc.borrow().open_x()
+    });
+    buttons_box.append(&x_button);
+
+    let software_button = Button::builder()
+    .label("Otwórz Sklep z aplikacjami")
+    .build();
+    software_button.connect_clicked({
+        let actions_rc = actions_rc.clone();
+        move |_| actions_rc.borrow().open_software()
+    });
+    buttons_box.append(&software_button);
+
+    let changelog_button = Button::builder()
+    .label("Otwórz Changelog")
+    .build();
+    changelog_button.connect_clicked({
+        let actions_rc = actions_rc.clone();
+        move |_| actions_rc.borrow().open_changelog()
+    });
+    buttons_box.append(&changelog_button);
+
+    let system_info_button = Button::builder()
+    .label("Otwórz Informacje o systemie")
+    .build();
+    system_info_button.connect_clicked({
+        let actions_rc = actions_rc.clone();
+        move |_| actions_rc.borrow().open_system_info()
+    });
+    buttons_box.append(&system_info_button);
+
+    let report_bug_button = Button::builder()
+    .label("Zgłoś błąd")
+    .build();
+    report_bug_button.connect_clicked({
+        let actions_rc = actions_rc.clone();
+        move |_| actions_rc.borrow().report_bug()
+    });
+    buttons_box.append(&report_bug_button);
+
+    let forum_button = Button::builder()
+    .label("Otwórz Forum dyskusyjne")
+    .build();
+    forum_button.connect_clicked({
+        let actions_rc = actions_rc.clone();
+        move |_| actions_rc.borrow().open_forum()
+    });
+    buttons_box.append(&forum_button);
+
+    let update_button = Button::builder()
+    .label("Aktualizuj system")
+    .build();
+    update_button.connect_clicked({
+        let actions_rc = actions_rc.clone();
+        move |_| actions_rc.borrow().update_system()
+    });
+    buttons_box.append(&update_button);
+
+    scrolled_window.set_child(Some(&buttons_box));
+    main_box.append(&scrolled_window);
+
+    // Separator
+    let separator2 = Separator::new(Orientation::Horizontal);
+    main_box.append(&separator2);
 
     // Footer
     let footer_label = Label::builder()
-        .label("© 2025 HackerOS Team | All rights reserved")
-        .halign(Align::Center)
-        .build();
-    footer_label.style_context().add_class("footer");
-    content_box.append(&footer_label);
+    .label("© 2025 HackerOS Team")
+    .css_classes(vec!["footer".to_string()])
+    .build();
+    main_box.append(&footer_label);
 
-    // ToolbarView to integrate header and content
-    let toolbar_view = ToolbarView::new();
-    toolbar_view.add_top_bar(&header_bar);
-    toolbar_view.set_content(Some(&content_box));
-
-    // Set window content
-    window.set_content(Some(&toolbar_view));
+    window.set_content(Some(&main_box));
 }
